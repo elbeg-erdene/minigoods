009eeb96630f10b7ccb5caf7303fc6cc79f74182
elbeg-erdene <85168532+elbeg-erdene@users.noreply.github.com>
2026-02-27 10:32:53 +0800

    Update CategoriesScreen.tsx

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Product, Category, SubCategory } from '../types';
 
 interface CategoriesScreenProps {
