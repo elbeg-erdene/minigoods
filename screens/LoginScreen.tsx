55d608611b3b4f8f563e7688a41345fecac22925
elbeg-erdene <85168532+elbeg-erdene@users.noreply.github.com>
2026-03-13 21:50:07 +0800

    Update LoginScreen.tsx

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -34,7 +34,7 @@ const response =
       if(!response.ok) { throw new Error("Network response not ok");}
 const result = await response.json();
       if(result.status !=="ok") {throw new Error("Login failed");}
-     await onLogin(cleanPhone);
+     onLogin(cleanPhone);
 
     } catch (error) {
       console.error(error);
